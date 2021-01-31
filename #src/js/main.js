8d244208e30ae51ca62e3030364176317b4165ee
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-01-31 19:40:53 +0200

    Fixed bugs

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,12 +1,3 @@
-function ibg() {
+// if you need slick slider uncomment this cat
 
-    $.each($('.ibg'), function (index, val) {
-        if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-        }
-    });
-}
-
-ibg();
-
-@@include('slick.min.js')
+// @@include('slick.min.js') 
