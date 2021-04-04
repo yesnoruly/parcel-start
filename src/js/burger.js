e959d6698a045bfe865c015e3e87328586eced2c
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-04-04 09:43:43 +0300

    Comment out the variable headerElement

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,12 @@
 
 const headerMenu    = document.querySelector(".header__menu");
 const headerBurger  = document.querySelector(".header__burger");
-const headerElement = document.querySelector(".header__link");
+// const headerElement = document.querySelector(".header__link");
 const body          = document.querySelector("body");
 
 
-headerBurger.addEventListener("click", function (evt) {
-    evt.preventDefault();
+headerBurger.addEventListener("click", function (e) {
+    e.preventDefault();
     headerMenu.classList.toggle("header-active");
     headerBurger.classList.toggle("header-active");
     body.classList.toggle("lock");
